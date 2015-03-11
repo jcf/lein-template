80e5eb2cabb2971959d89af2324a9e8099306322
James Conroy-Finn <james@logi.cl>
2015-03-11 12:17:25 +0000

    Add Leiningen to dev dependencies

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://opensource.org/licenses/MIT"}
   :deploy-repositories [["releases" :clojars]]
   :eval-in-leiningen true
-  :profiles {:dev {:dependencies [[me.raynes/fs "1.4.6"]]}})
+  :profiles {:dev {:dependencies [[leiningen "2.5.1"]
+                                  [me.raynes/fs "1.4.6"]]}})
