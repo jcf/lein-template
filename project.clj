f4887328e34363de73b69dd8069962b3d2c90c7a
James Conroy-Finn <james@logi.cl>
2015-04-27 22:40:47 +0100

    Version 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jcf/lein-template "0.1.1-SNAPSHOT"
+(defproject jcf/lein-template "0.1.1"
   :description "FIXME: write description"
   :url "https://github.com/jcf/lein-template"
   :license {:name "The MIT License"
