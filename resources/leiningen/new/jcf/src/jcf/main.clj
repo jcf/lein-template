cf12bfae8e842400881d58828d10d519c0bd1292
James Conroy-Finn <james@logi.cl>
2015-08-04 21:01:29 +0100

    Use Schema to validate/coerce configuration

--- resources/leiningen/new/jcf/src/jcf/main.clj
+++ resources/leiningen/new/jcf/src/jcf/main.clj
@@ -1,12 +1,10 @@
 (ns {{ns}}.main
   (:gen-class)
   (:require [com.stuartsierra.component :as component]
-            [environ.core :refer [env]]
             [{{ns}}.config :as config]
             [{{ns}}.system :refer [new-system]]))
 
 (defn -main []
-  (-> env
-      config/config-map
+  (-> config/config-map
       new-system
       component/start-system))
