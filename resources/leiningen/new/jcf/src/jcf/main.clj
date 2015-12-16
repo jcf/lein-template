b8931d848b7af8cff9f9e57bf0748b832cf6cc0a
James Conroy-Finn <james@logi.cl>
2015-12-16 21:25:16 +0000

    Pass env through to config-map inside main

--- resources/leiningen/new/jcf/src/jcf/main.clj
+++ resources/leiningen/new/jcf/src/jcf/main.clj
@@ -1,12 +1,14 @@
 (ns {{ns}}.main
   (:gen-class)
   (:require [com.stuartsierra.component :as component]
+            [environ.core :refer [env]]
             [{{ns}}.config :as config]))
 
 (defn new-system [config]
   (component/system-map :config-options config))
 
 (defn -main []
-  (-> config/config-map
+  (-> env
+      config/config-map
       new-system
       component/start-system))
