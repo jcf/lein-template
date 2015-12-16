14a9c2e2bd3cad3d497e64955fde58c193e11e63
James Conroy-Finn <james@logi.cl>
2015-12-16 21:27:46 +0000

    Define :main for all profiles

--- resources/leiningen/new/jcf/project.clj
+++ resources/leiningen/new/jcf/project.clj
@@ -5,6 +5,7 @@
                  [environ "1.0.0"]
                  [org.clojure/clojure "1.7.0"]
                  [prismatic/schema "0.4.3"]]
+  :main {{ns}}.main
   :min-lein-version "2.5.0"
   :uberjar-name "{{hyphenated-name}}-standalone.jar"
   :profiles
@@ -12,5 +13,4 @@
                         [reloaded.repl "0.1.0"]]
          :source-paths ["dev"]}
    :uberjar {:aot :all
-             :main {{ns}}.main
              :omit-source true}})
